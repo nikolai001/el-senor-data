6d3afba789e2c6e1b95bc795d2f7de5dcf5d8334
nikolairaahauge <nira36696@edu.ucl.dk>
2023-02-25 16:59:58 +0100

    Pulling data from the right region compared to my location

--- src/Charts.js
+++ src/Charts.js
@@ -12,16 +12,16 @@ function setApi() {
     if (currentDate.getMonth() < 10 && currentDate.getDate() < 10 ) {
       let month =  '0' + (currentDate.getMonth()+1)
       let day = '0' + (currentDate.getDate())
-      return sourceLink+currentDate.getFullYear() + '/' + month+'-'+day+'_'+'DK2'+'.json'
+      return sourceLink+currentDate.getFullYear() + '/' + month+'-'+day+'_'+'DK1'+'.json'
     }else if (currentDate.getMonth() < 10 && currentDate.getDate() >= 10) {
       let month =  '0' + (currentDate.getMonth()+1)
-      return sourceLink+currentDate.getFullYear() + '/' + month+'-'+currentDate.getDate()+'_'+'DK2'+'.json'
+      return sourceLink+currentDate.getFullYear() + '/' + month+'-'+currentDate.getDate()+'_'+'DK1'+'.json'
     } else {
         let day = '0' + (currentDate.getDate())
-        return sourceLink+currentDate.getFullYear() + '/' + currentDate.month+'-'+day+'_'+'DK2'+'.json'
+        return sourceLink+currentDate.getFullYear() + '/' + currentDate.month+'-'+day+'_'+'DK1'+'.json'
     }
   }
-  return sourceLink+currentDate.getFullYear() + '/' + currentDate.getMonth()+'-'+currentDate.getDate()+'_'+'DK2'+'.json'
+  return sourceLink+currentDate.getFullYear() + '/' + currentDate.getMonth()+'-'+currentDate.getDate()+'_'+'DK1'+'.json'
 }
 
 
