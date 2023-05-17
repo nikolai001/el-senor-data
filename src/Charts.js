be2bd5d47692d716921b2adf973a4e4f386c932f
nikolairaahauge <nira36696@edu.ucl.dk>
2023-05-17 21:06:43 +0200

    Rewrote table of data to use grid, made versions for tablet and desktop

--- src/Charts.js
+++ src/Charts.js
@@ -44,43 +44,50 @@ function Charts() {
 
   return (
     <main className='chart'>
-      <label className='chart__date-label' htmlFor="Date">Vælg dato</label>
-      <input name="Date" className='chart__date-picker' type="date" min={"2022-10-27"} max={maxDate()} value={localDate.toISOString().split('T')[0]} onChange={(e) => setLocalDate(new Date(e.target.value))}></input>
-      <label className='chart__region-label' htmlFor="Region">Vælg landsdel</label>
-      <select className='chart__region' name="Region" onChange={({target:{value}}) => regionChange(value)}>
-        <option className='region__option' value="DK2">Østdanmark</option>
-        <option className='region__option' value="DK1">Vestdanmark</option>
-      </select>
-      <table className='chart__table'>
-          <tr className='table__row'>
-              <th className='row__head'>
+      
+      <div className='chart__date'>
+        <label className='date__label' htmlFor="Date">Vælg dato</label>
+        <input name="Date" className='date__picker' type="date" min={"2022-10-27"} max={maxDate()} value={localDate.toISOString().split('T')[0]} onChange={(e) => setLocalDate(new Date(e.target.value))}></input>
+      </div>
+      
+      <div className='chart__region'>
+        <label className='region__label' htmlFor="Region">Vælg landsdel</label>
+        <select className='region__picker' name="Region" onChange={({target:{value}}) => regionChange(value)}>
+          <option className='picker__option' value="DK2">Østdanmark</option>
+          <option className='picker__option' value="DK1">Vestdanmark</option>
+        </select>
+      </div>
+
+      <article className='chart__table'>
+          <div className='table__row'>
+              <div className='row__head'>
                   From
-              </th>
-              <th className='row__head'>
+              </div>
+              <div className='row__head'>
                   Rå pris
-              </th>
-              <th className='row__head'>
+              </div>
+              <div className='row__head'>
                   Pris med moms
-              </th>
-          </tr>
+              </div>
+          </div>
 
           {apiData.map((entry) => 
-              <tr className='table__row' key={entry.time_start}>
-                  <td className='row__column row__column--time'>
+              <div className='table__row' key={entry.time_start}>
+                  <div className='row__column row__column--time'>
                       <p className='column--time__text'>{(entry.time_start.substring(0,2) > '05' && entry.time_start.substring(0,2) < '18') ? <i className="material-symbols-outlined column--time__symbol column--time__symbol--light">light_mode</i> : <i className="material-symbols-outlined column--time__symbol column--time__symbol--dark">dark_mode</i>}
                      <span className='column--time-time'>{entry.time_start}</span></p>
-                  </td>
+                  </div>
 
-                  <td className='row__column'>
+                  <div className='row__column'>
                   {(entry.DKK_per_kWh.charAt(0) === '0' ? <p>{(entry.DKK_per_kWh*100).toFixed(2).replace('.',',').concat(' Øre')}</p> : <p>{(entry.DKK_per_kWh*1).toFixed(2).replace('.',',').concat(' Kr')}</p>)}
-                  </td>
+                  </div>
 
-                  <td className='row__column'>
+                  <div className='row__column'>
                       {((entry.DKK_per_kWh * 1.25).toString().charAt(0) === '0' ? <p>{((entry.DKK_per_kWh * 1.25)*100).toFixed(2).replace('.',',').concat(' Øre')}</p> : <p>{(entry.DKK_per_kWh * 1.25).toFixed(2).replace('.',',').concat(' Kr')}</p>)}
-                  </td>
-              </tr>
+                  </div>
+              </div>
           )} 
-      </table>
+      </article>
     </main>
   );
 }
